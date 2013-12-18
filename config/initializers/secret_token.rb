27f6e08b1636747e49dd9e3179349d48dd5eaede
tystube <tystube@gmail.com>
2013-12-17 17:45:47 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leva::Application.config.secret_key_base = '1056fa92caccdaaf1b425a55e4a4f0e20f61a3074aaa9ccf686ec4172e5c3c43dc792cc072519c820884920151b5cc39732b945572d39f9bf03b7216f604facb'
